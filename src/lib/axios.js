a9e40bb9904ccb231b37bef292673a0d9e97ef3e
stoimart2020-cmd <stomart2020@gmail.com>
2026-03-14 15:07:38 +0530

    fix production api url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "/api",
   // baseURL: "https://stoi-server.onrender.com",
   withCredentials: true,
 });
