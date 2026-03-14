014cf55f8e465acf0ae10b20d1281223776e4318
stoimart2020-cmd <stomart2020@gmail.com>
2026-03-14 15:11:07 +0530

    fix axios baseURL correctly for both local and production

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_BASE_URL || "",
   // baseURL: "https://stoi-server.onrender.com",
   withCredentials: true,
 });
